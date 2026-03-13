fb03033e8e17c71fce4ec8a11d1560dda4e4bd7c
spskills <nandinhaa1280@gmail.com>
2026-03-13 10:51:35 -0300

    feito exercicio 3

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-let nome = document.getElementById("nome");
+let item = document.getElementById("item");
 let botaoEnviar = document.getElementById("btnEnviar");
 let mensagem = document.getElementById("mensagem");
+let ul = document.querySelector(".lista");
 
-botaoEnviar.addEventListener("click", ()=>{
-    
-    if (Number(nome.value) || nome.value == ""){
+botaoEnviar.addEventListener("click", () => {
+
+    //validação do valor inserido
+    if (Number(item.value) || item.value == "") {
         mensagem.innerText = `Error`
 
     } else {
-        mensagem.innerText = `Olá ${nome.value}`;
+
+        //cria a lista
+        let lista = document.createElement("li");
+        ul.appendChild(lista);
         
+        //cria novo item da lista
+        let novoItem = document.createElement("p");
+        novoItem.innerText = item.value;
+        lista.appendChild(novoItem);
     }
 })
